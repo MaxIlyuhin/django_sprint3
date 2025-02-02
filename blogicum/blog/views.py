a0b36bb9125ca34a69c65990518f13cf392c0675
Maxim Ilyukhin <iliuhin.maxim2016@yandex.ru>
2025-02-02 13:09:59 +0300

    Second part of project Blogicum is done.

---
+++ blogicum/blog/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from blog.models import Post, Category
+
+
+# Create your views here.
+def index(request):
+    """Главная страница: выводит 5 последних опубликованных постов."""
+    template_name = 'blog/index.html'
+    post_list = Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    context = {
+        'post_list': post_list,
+    }
+    return render(request, template_name, context)
+
+
+def post_detail(request, id):
+    """Страница отдельной публикации."""
+    template_name = 'blog/detail.html'
+    post = get_object_or_404(
+        Post.objects.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True
+        ).select_related('category'),
+        id=id
+    )
+
+    context = {'post': post}
+    return render(request, template_name, context)
+
+
+def category_posts(request, category_slug):
+    """Страница категории: показывает посты выбранной категории."""
+    template_name = 'blog/category.html'
+    category = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    post_list = Post.objects.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category=category
+    ).order_by('-pub_date')
+
+    context = {
+        'category': category,
+        'post_list': post_list
+    }
+    return render(request, template_name, context)
